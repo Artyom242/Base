40974286c94f58268bac02489611ab6fce922d80
Artyom242 <artemp876@gmail.com>
2023-02-05 21:48:42 +0700

    удаление ненужных файлов

--- config/add.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'config/connect.php';
-$db = new PDO('mysql:host=localhost;dbname=Users', 'root', '');
-$sql = "insert into Users('Name','Age',Salary) values (name,age,salary)";
-$result = $db->prepare($sql);
-
-header('/Location: /');
--- config/configDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost'; // имя хоста
-$user = 'root';      // имя пользователя
-$pass = '';          // пароль
-$name = 'Users';      // имя базы данных
-
-
-$db = new pdo('mysql:host=localhost;dbname=Users', 'root', '');
